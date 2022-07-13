1c37d119fd2772bf98d5d91f06fd1d48605d7cc7
Phantom-Farmer <joeljdahl@gmail.com>
2022-07-12 19:54:52 -0500

    word up

--- components/JokeGenerator.js
+++ components/JokeGenerator.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import { useState } from 'react';
 import getJoke from '../api/jokeData';
 
 function Joker() {
+  const [tell, setTell] = useState();
+  const [laugh, setLaugh] = useState();
   const clicker1 = () => {
-    getJoke().then((joke) => (joke.setup));
+    getJoke().then((joke) => setTell(joke.setup));
   };
+  const clicker2 = () => {
+    getJoke().then((joke) => setLaugh(joke.delivery));
+  };
+
   return (
     <>
+      <h1>{tell}</h1>
+      <h1>{laugh}</h1>
       <button type="button" onClick={clicker1}>Get a Joke</button>
+      <button type="button" onClick={clicker2}>Bahahahaha!</button>
     </>
   );
 }
